355c387814ffa5a2096a9c6cdc478dbd745e9294
Eduardo0022 <128662240+Eduardo0022@users.noreply.github.com>
2025-09-10 23:24:13 +0000

    commit

--- console.js
+++ console.js
@@ -4,9 +4,13 @@ let nome = "Eduardo";
 let idade = 16;
 let altura = 1.76;
 let estudante = true;
+let peso = 70
+let sobrenome = "Vergara Fuhrmann"
 
 console.log("Nome: ", nome)
 console.log("Idade: ", idade)
 console.log("Altura: ", altura)
 console.log("Estudante: ", estudante)
+console.log("Peso: ", peso)
 
+console.log(`Nome: ${nome} ${sobrenome}, Idade: ${idade}, com peso de ${peso} e a altura de ${altura}`)
