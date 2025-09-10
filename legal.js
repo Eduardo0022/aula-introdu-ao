04fc9ee7b302d75e70488db78d74efbcf1deb486
Eduardo0022 <128662240+Eduardo0022@users.noreply.github.com>
2025-09-10 23:30:09 +0000

    commit 2

---
+++ legal.js
@@ -0,0 +1,15 @@
+let textp = "Hello World";
+let nulo = null;
+let numero = 42;
+let logico = false;
+let decimal = 3.14
+let indefinido 
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof decimal);
+console.log(typeof logico);
+console.log(typeof nulo);
+console.log(typeof indefinido);
+
+
