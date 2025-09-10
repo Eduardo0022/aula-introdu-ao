e259500b09892e0b31eff10cfb273c69e6987be0
Eduardo0022 <128662240+Eduardo0022@users.noreply.github.com>
2025-09-10 20:01:49 -0300

    Update console.js

---
+++ console.js
@@ -0,0 +1,12 @@
+console.log("Hello, World")
+
+let nome = "Eduardo";
+let idade = 16;
+let altura = 1.76;
+let estudante = true;
+
+console.log("Nome: ", nome)
+console.log("Idade: ", idade)
+console.log("Altura: ", altura)
+console.log("Estudante: ", estudante)
+
