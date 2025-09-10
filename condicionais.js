665a7530e71e44f9d0f9ef23cad17b6dcdaa7eb4
Eduardo0022 <128662240+Eduardo0022@users.noreply.github.com>
2025-09-10 23:48:19 +0000

    condicionais

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+/*
+
+se (nota >= 7.0) {
+    escreva("Aprovado!")
+}senao{ 
+escreva("Reprovado")
+}
+
+*/
+
+let nota = 0
+
+if (nota >= 7.0) {
+    console.log("Aprovado!");
+}else { 
+console.log("Reprovado");
+}
---
+++ exercicio-pratico-um.js
@@ -0,0 +1,8 @@
+let meuNome = "Eduardo Vergara";
+let minhaIdade = 16;
+let minhaCidade = "Pomerode - SC";
+let gostadeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos!`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosto de programar: ", gostadeProgramar);
